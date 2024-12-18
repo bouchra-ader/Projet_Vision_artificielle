20a8e8767a966ffe0e781a5678a4e30cc16671a7
bouchra-ader <bouchraaderdour20@gmail.com>
2024-12-18 02:10:16 +0100

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,24 +1,28 @@
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 class DataPreprocessor:
-    def __init__(self, train_dir, validation_dir, img_size=(150, 150), batch_size=32):
+    def __init__(self, train_dir, validation_dir):
         self.train_dir = train_dir
         self.validation_dir = validation_dir
-        self.img_size = img_size
-        self.batch_size = batch_size
 
-    def get_loaders(self):
-        transform = transforms.Compose([
-            transforms.Resize(self.img_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+    def get_generators(self):
+        # Use ImageDataGenerator to preprocess the images
+        train_datagen = ImageDataGenerator(rescale=1./255)
+        validation_datagen = ImageDataGenerator(rescale=1./255)
 
-        train_dataset = datasets.ImageFolder(root=self.train_dir, transform=transform)
-        validation_dataset = datasets.ImageFolder(root=self.validation_dir, transform=transform)
+        train_generator = train_datagen.flow_from_directory(
+            self.train_dir,
+            target_size=(150, 150),  # Resize images
+            batch_size=32,
+            class_mode='binary'  # For binary classification (cat vs dog)
+        )
 
-        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        validation_loader = DataLoader(validation_dataset, batch_size=self.batch_size, shuffle=False)
+        validation_generator = validation_datagen.flow_from_directory(
+            self.validation_dir,
+            target_size=(150, 150),
+            batch_size=32,
+            class_mode='binary'
+        )
 
-        return train_loader, validation_loader
+        return train_generator, validation_generator
